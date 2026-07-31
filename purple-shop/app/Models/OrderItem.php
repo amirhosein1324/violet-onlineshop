3e6840d566b5935328681424691eebc4d588a406
amirhosein <amirhoseinparsa1234@gmail.com>
2026-07-31 03:51:38 +0330

    new changes

--- purple-shop/app/Models/OrderItem.php
+++ purple-shop/app/Models/OrderItem.php
@@ -20,4 +20,9 @@ class OrderItem extends Model
     {
         return $this->belongsTo(Product::class);
     }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
